283f282bed3a25fe7fc5bd8004f2000b8baab576
Damilola Michael Ige <idm.calculus@gmail.com>
2022-01-28 15:00:50 +0100

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by IGE DAMILOLA MICHAEL on 28/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
